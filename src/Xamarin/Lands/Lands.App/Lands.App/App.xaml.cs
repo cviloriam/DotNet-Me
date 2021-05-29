4e495def153b441ac2bcdb34ea395d37b4647c9d
Carlos Antonio Viloria Madera <cviloriam@hotmail.com>
2021-05-28 22:03:47 -0500

    Adicionar el proyecto Land desarrollado en Xamarin

---
+++ src/Xamarin/Lands/Lands.App/Lands.App/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Lands.App.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Lands.App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new LoginPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
